d297685ddcd1d4d56649c344cb764554b856aae9
S.V.Chanupa Hansaja Vithanage <vithanagesvch.23@uom.lk>
2026-08-20 08:47:56 +0530

    Update API_URL to new deployment URL

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  */
 window.APP_CONFIG = {
   // Paste the /exec URL from Apps Script > Deploy > New deployment
-  API_URL: 'https://script.google.com/macros/s/AKfycbxbmmX9QwrZ2vpmCVhQzGaf5N16xT_BzwZh65XY5zixkH5bcFuybMmQjc8k-PRNdKf7/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzMBHbTyZPnO0OIX603dAUDTV7EIeLlqXqOnfWLWMWLrq5ttluD_P-vrvr57Zlj7yjf/exec',
 
   // Must match CONFIG.API_TOKEN in apps-script/Code.gs
   API_TOKEN: 'abcdefghijklmnop',
